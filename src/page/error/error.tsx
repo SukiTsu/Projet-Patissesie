e69ab9280c0f24514da3a08880afa11ff5a29673
SukiTsu <buibrandon47@gmail.com>
2024-10-24 15:16:32 +0200

    Correction de l'oublie du changement de changeSaison sur la page error

--- src/page/error/error.tsx
+++ src/page/error/error.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../../components/navbar';
 import Footer from '../../components/footer';
 import ChangeStyleSaison from '../../components/changeStyleSaison';
 
 const NotFound = () => {
+  const [isCheck, setIsCheck] = useState(() => {
+    const saved = localStorage.getItem('toggleState');
+    return saved !== null ? JSON.parse(saved) : false;
+  });
+
+  const handleCheckChange = (newState: boolean | ((prevState: boolean) => boolean)) => {
+      setIsCheck(newState);
+  };
+
   return (
     <div>
         <Navbar />
-        <ChangeStyleSaison />
-        <div className='content'>
+        <ChangeStyleSaison onCheckChange={handleCheckChange}/>
+        <div className={`${isCheck ? 'festive' : 'seasonal'} body`}>
            <h1>404 - Page non trouvé :(</h1>
             <p>Oops! Il se trouve que la page que vous essayez de trouver n'existe pas.</p>
             <Link to="/">Revenir à la page d'acceuil</Link>
