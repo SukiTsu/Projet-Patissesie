dbcd17f6524f275fedd6fab5b8cd7c8543a6d65b
SukiTsu <buibrandon47@gmail.com>
2024-10-30 22:06:21 +0100

    Ajout d'un menu burger pour la navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,37 +1,46 @@
 import { Link } from "react-router-dom";
 import '../assets/style/navbar.css'
 import cakeImage from '../assets/logo.png';
+import { useState } from "react";
 
-export default function Navbar() {
+export default function Navbar(): JSX.Element {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-      <nav className="navbar">
+        <nav className="navbar">
+            <img src={cakeImage} alt="Logo Atelier-Dharma" className="navbar-logo" />
 
-        <img src={cakeImage} alt="Logo Atelier-Dharma" className="navbar-logo"/>
+            <ul className={`navbar-menu ${isMenuOpen ? 'open' : ''}`}>
+                <li>
+                    <Link to="/" className="link-navbar">
+                        <strong>Home</strong>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/shop" className="link-navbar">
+                        <strong>Shop</strong>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/cake-design" className="link-navbar">
+                        <strong>Cake-design</strong>
+                    </Link>
+                </li>
+            </ul>
 
-        <ul className="navbar-menu">
-          <li>
-            <Link to="/" className="link-navbar">
-              <strong>Home</strong>
-            </Link>
-          </li>
-          <li>
-            <Link to="/shop" className="link-navbar">
-              <strong>Shop</strong>
-            </Link>
-          </li>
-          <li>
-            <Link to="/cake-design" className="link-navbar">
-              <strong>Cake-design</strong>
-            </Link>
-          </li>
-        </ul>
+            <div className="navbar-contact">
+                <Link to="/contact" className="link-navbar">
+                    <strong>Contact</strong>
+                </Link>
+            </div>
 
-        <div className="navbar-contact">
-          <Link to="/contact" className="link-navbar">
-            <strong>Contact</strong>
-          </Link>
-        </div>
-
-      </nav>
+            <button className="menu-toggle" onClick={toggleMenu}>
+                &#9776;
+            </button>
+        </nav>
     );
-  }
+}
